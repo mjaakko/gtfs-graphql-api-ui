f1deaafee1c558e5db2473953f56cee5bced5b97
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-07-15 13:06:24 +0300

    Create Apollo client with subscription support

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8081/graphql'
+})
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:8081/graphql',
+}))
+
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
