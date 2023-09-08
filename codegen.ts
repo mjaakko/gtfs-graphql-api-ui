30d32d7983acd569dcc4c2233ee1ad4371a3dff9
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-09-08 22:22:42 +0300

    Add support for generating TypeScript types for GraphQL schema

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+
+const config: CodegenConfig = {
+  schema: 'http://localhost:8081/graphql',
+  documents: ['src/**/*.tsx'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+      }
+    }
+  },
+  ignoreNoDocuments: true,
+};
+
+
+export default config;
