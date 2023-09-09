174bf430e7e8693e77df0494bb3cffea57acae50
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-09-09 15:49:12 +0300

    Create hook for listening vehicle positions

---
+++ src/hooks/useVehiclePositions.ts
@@ -0,0 +1,41 @@
+import { useSubscription } from "@apollo/client";
+
+import { gql } from "../__generated__/gql";
+
+const VEHICLE_POSITIONS_SUBSCRIPTION = gql(`
+  subscription VehiclePositions {
+    vehiclePositions {
+        vehicleId
+        vehicleLabel
+        currentStop {
+            arrivalTimeScheduled
+            departureTimeScheduled
+            headsign
+            stop {
+                name
+            }
+        }
+        trip {
+            tripId
+            date
+            route {
+                longName
+            }
+            headsign
+        }
+        latitude
+        longitude
+        bearing
+        speed
+        status
+        timestamp
+    }
+  }
+`);
+
+
+const useVehiclePositions = () => {
+    return useSubscription(VEHICLE_POSITIONS_SUBSCRIPTION)
+}
+
+export default useVehiclePositions
