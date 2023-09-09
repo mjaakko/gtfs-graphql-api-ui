2d36960c9d311488ab0cabf3e94602b69e3aa3f3
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-09-10 00:31:57 +0300

    Create hook for fetching stop schedule rows

---
+++ src/hooks/useStopSchedule.ts
@@ -0,0 +1,33 @@
+import { useQuery } from "@apollo/client"
+
+import { gql } from "../__generated__";
+
+const STOP_SCHEDULE_ROWS_QUERY = gql(`
+    query StopScheduleRows($stopId: String!, $maxRows: Int!) {
+        stop(id: $stopId) {
+            scheduleRows(max: $maxRows) {
+                trip {
+                    tripId
+                    date
+                    headsign
+                }
+                headsign
+                arrivalTimeScheduled
+                departureTimeScheduled
+                pickUp
+                dropOff
+            }
+        }
+    }
+`);
+
+const useStopScheduleRows = (stopId: string, maxRows: number) => {
+    return useQuery(STOP_SCHEDULE_ROWS_QUERY, {
+        variables: {
+            stopId: stopId,
+            maxRows: maxRows
+        }
+    })
+}
+
+export default useStopScheduleRows
