1124f36599263fb562011fc90a56f4e328ce89e1
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-09-25 22:40:37 +0300

    Create component for showing stop schedule rows

---
+++ src/components/StopScheduleRows.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+import { CircularProgress } from "@mui/material"
+
+import { StopScheduleRow } from "../__generated__/graphql"
+
+import useStopScheduleRows from "../hooks/useStopSchedule"
+
+import { formatTime } from "../utils/timeFormat"
+
+
+const StopScheduleRows = (props: { stopId: string, timezone?: string }) => {
+    const { loading, data } = useStopScheduleRows(props.stopId, 4)
+  
+    if (loading) {
+      return <CircularProgress sx={{ m: 2 }}/>
+    }
+    
+    if (data) {
+      return <ul>
+        { 
+          (data.stop?.scheduleRows as StopScheduleRow[]).map((scheduleRow: StopScheduleRow) => {
+            const headsign = scheduleRow.headsign || scheduleRow.trip.headsign
+  
+            const timezone = props.timezone || scheduleRow.trip.route.agency?.timezone || "Europe/Helsinki"
+
+            return (
+              <li key={`${scheduleRow.trip.tripId}_${scheduleRow.trip.date}`}>
+                { formatTime(scheduleRow.departureTimeScheduled, timezone) } - <i>{ headsign }</i>
+              </li>
+            )
+          })
+        }
+      </ul>
+    }
+  
+    return null
+  }
+
+export default StopScheduleRows
