af0db7b0ca618c59f5deb7679e54f089c404caee
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-10-21 21:00:26 +0300

    Create hook for using trip details

---
+++ src/hooks/useTripDetails.ts
@@ -0,0 +1,41 @@
+import { useQuery } from "@apollo/client"
+
+import { gql } from "../__generated__";
+
+const TRIP_DETAILS_QUERY = gql(`
+    query TripScheduleRows($tripId: String!, $date: Date!) {
+        trip(id: $tripId, date: $date) {
+            route {
+              agency {
+                timezone
+              }
+              shortName
+              longName
+            }
+            scheduleRows {
+              sequenceNumber
+              stop {
+                name
+                timezone
+              }
+              arrivalTimeScheduled
+              departureTimeScheduled
+            }
+            vehiclePosition {
+              vehicleLabel
+            }
+        }
+    }
+`);
+
+const useTripDetails = (tripId: string, date: string) => {
+    return useQuery(TRIP_DETAILS_QUERY, {
+        variables: {
+            tripId: tripId,
+            date: date
+        },
+        fetchPolicy: "no-cache" 
+    })
+}
+
+export default useTripDetails
