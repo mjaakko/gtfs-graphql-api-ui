5009f2dc8dd811af5f9d5733ecc42413f032a637
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-10-21 11:17:13 +0300

    Create utility function for formatting date times

--- src/utils/timeFormat.ts
+++ src/utils/timeFormat.ts
@@ -4,4 +4,8 @@ function formatTime(timeStr: string, timezone: string): string {
     return dayjs(timeStr).tz(timezone).format("LT")
 }
 
-export { formatTime }
+function formatDateTime(timeStr: string, timezone: string): string {
+    return dayjs(timeStr).tz(timezone).format("L LT")
+}
+
+export { formatTime, formatDateTime }
