30b7465504b67955ac69628fb8e9d72770abfbc4
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-11-05 00:15:14 +0200

    Open stop details page from stop marker

--- src/components/StopMarker.tsx
+++ src/components/StopMarker.tsx
@@ -1,14 +1,31 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Marker, Popup } from "react-leaflet"
+import { Marker as LeafletMarker } from "leaflet"
 
 import { Stop } from "../__generated__/graphql"
 
-import StopScheduleRows from "./StopScheduleRows"
+import { useNavigate, useMatch } from "react-router-dom"
+import { Typography } from "@mui/material"
 
 const StopMarker = (props: { stop: Stop }) => {
     const [showScheduleRow, setShowScheduleRows] = useState(false)
   
     const stop = props.stop
+
+    const navigate = useNavigate()
+
+    const markerRef = useRef<LeafletMarker>(null)
+
+    const pathMatch = useMatch("/stops/:stopId")
+
+    const markerOpen = pathMatch?.params.stopId === stop.stopId
+
+    useEffect(() => {
+      if (markerRef.current && !markerRef.current.isPopupOpen() && markerOpen) {
+        markerRef.current.openPopup()
+      }
+    }, [markerOpen])
+
     if (!stop.latitude || !stop.longitude) {
       //Can't create markers for stops without location
       return null
@@ -18,12 +35,21 @@ const StopMarker = (props: { stop: Stop }) => {
       <Marker
         position={[stop.latitude, stop.longitude]}
         eventHandlers={{
-          popupclose: () => setShowScheduleRows(false),
-          popupopen: () => setShowScheduleRows(true)
-        }}>
+          popupclose: () => {
+            //If marker ref is null, the map hasn't loaded yet and we should ignore this event, because we don't want to close the popup which was opened programmatcally 
+            if (markerRef.current) {
+              navigate("/")
+            }
+          },
+          popupopen: () => {
+            if (!markerOpen) {
+              navigate(`/stops/${stop.stopId}`)
+            }
+          }
+        }}
+        ref={markerRef}>
         <Popup minWidth={200}>
-          <b>{ stop.name }</b>
-          { showScheduleRow && <StopScheduleRows stopId={stop.stopId} timezone={stop.timezone || undefined} />}
+          <Typography variant="subtitle2" component="span">{ stop.name }</Typography>
         </Popup>
       </Marker>
     )
